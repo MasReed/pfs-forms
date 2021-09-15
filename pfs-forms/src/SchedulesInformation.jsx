c6db57320559e077d6a937494e0db5ed5a640a43
MasReed <bluekitdrummer@gmail.com>
2021-09-14 19:32:41 -0500

    implement functionality for handling table cell updates in datagrid

--- pfs-forms/src/SchedulesInformation.jsx
+++ pfs-forms/src/SchedulesInformation.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
 import ScheduleOne from './assetSchedules/ScheduleOne'
--- pfs-forms/src/assetSchedules/ScheduleOne.jsx
+++ pfs-forms/src/assetSchedules/ScheduleOne.jsx
@@ -12,35 +12,33 @@ const useStyles = makeStyles({
     color: 'primary',
     height: 300,
     width: '100%',
-    minWidth: 1000,
+    minWidth: 1005,
   },
 })
-
-
 
 const columns: GridColDef[] = [
   {
     field: 'owner',
     headerName: 'Account Owner',
-    width: 150,
+    width: 250,
     editable: true
   },
   {
     field: 'description',
     headerName: 'Account Description',
-    width: 150,
+    width: 250,
     editable: true
   },
   {
     field: 'bank',
     headerName: 'Bank Name',
-    width: 150,
+    width: 250,
     editable: true
   },
   {
     field: 'balance',
     headerName: 'Balance',
-    width: 150,
+    width: 250,
     editable: true
   }
 ]
@@ -49,9 +47,9 @@ export default function ScheduleOne() {
   const classes = useStyles()
 
   const [rows: GridRowsProp, setRows] = useState([
-    { id: 1, owner: 'hello', description: 'world', bank: 'biwa', balance: 59999 },
-    { id: 2, owner: 'hello', description: 'world', bank: 'biwa', balance: 59999 },
-    { id: 3, owner: 'hello', description: 'world', bank: 'biwa', balance: 59999 },
+    { id: 1, owner: 'hllo', description: 'wrld', bank: 'biwa', balance: 59999 },
+    { id: 2, owner: 'helo', description: 'wrld', bank: 'biwa', balance: 59999 },
+    { id: 3, owner: 'helo', description: 'wrld', bank: 'biwa', balance: 59999 },
   ])
 
   const handleAddRow = (event) => {
@@ -59,7 +57,7 @@ export default function ScheduleOne() {
 
     setRows(
       [
-        ...rows,
+        ...(rows: GridRowsProp),
         {
           id: rows.length + 1,
           owner: '',
@@ -71,24 +69,46 @@ export default function ScheduleOne() {
     )
   }
 
+  const handleCellUpdate = (event) => {
+    console.log('event', event)
+    console.log('rows', rows)
+
+    const updatedRow = rows.find(row => row.id === event.id)
+    console.log('updateRow', updatedRow)
+
+    const updated = {
+      ...updatedRow,
+      [event.field]: event.value
+    }
+    console.log('updated', updated)
+
+
+    const updatedState = rows.map(row => row.id !== updatedRow.id ? row : updated)
+    console.log('updatedState', updatedState)
+
+    setRows(updatedState)
+
+  }
+
   return (
     <>
       <Typography variant='h5'>Schedule 1</Typography>
       <Typography variant='subtitle1'>Cash In Bank Accounts</Typography>
 
-      <div className={classes.table}>
+      <Box className={classes.table}>
         <DataGrid
           rows={rows}
           columns={columns}
           autoHeight={false}
           autoPageSize={false}
+          hideFooter={true}
+          onCellEditCommit={handleCellUpdate}
         />
+      </Box>
 
-        <Button variant='contained' color='primary' onClick={handleAddRow}>
-          Add Row
-        </Button>
-      </div>
-
+      <Button variant='contained' color='primary' onClick={handleAddRow}>
+        Add Row
+      </Button>
     </>
   )
 }
