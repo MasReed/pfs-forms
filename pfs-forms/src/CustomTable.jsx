47791397bfc0f400eb39823106da8492374f2367
MasReed <bluekitdrummer@gmail.com>
2021-09-16 16:34:19 -0500

    update table style; generate table headings from props

--- pfs-forms/src/CustomTable.jsx
+++ pfs-forms/src/CustomTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
+import Checkbox from '@material-ui/core/Checkbox'
+import Paper from '@material-ui/core/Paper'
 import TableContainer from '@material-ui/core/TableContainer'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -8,17 +10,22 @@ import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import TableFooter from '@material-ui/core/TableFooter'
-
-
-import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
-  tableContainer: {
+  container: {
+    borderRadius: '4px',
     margin: '3rem 0',
-    padding: '1rem 0'
+    padding: '0',
+  },
+  table: {
+
   },
   head: {
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main,
+  },
+  headingText: {
+    color: '#fff'
   },
   body: {
     backgroundColor: '#ccc'
@@ -28,18 +35,37 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function CustomTable ({ rows, columnHeadings }) {
+export default function CustomTable ({ rows, colHeadings }) {
   const classes = useStyles()
 
   return (
-    <TableContainer className={classes.tableContainer}>
-      <Table>
+    <TableContainer className={classes.container} component={Paper}>
+      <Table className={classes.table}>
         {/* Table Header */}
         <TableHead className={classes.head}>
           <TableRow>
-            <TableCell>
-              HEAD
+            {/* Select All Checkbox */}
+            <TableCell padding='checkbox' variant='head'>
+              <Checkbox
+                checked={true}
+                onChange={() => console.log('checkbox onchange called')}
+              />
             </TableCell>
+
+            {/* Generate Column Headings */}
+            {
+              colHeadings.map(heading => (
+                <TableCell key={`column-heading-${heading}`}>
+                  <Typography
+                    className={classes.headingText}
+                    variant='subtitle1'
+                  >
+                    {heading}
+                  </Typography>
+                </TableCell>
+              ))
+            }
+
           </TableRow>
         </TableHead>
 
