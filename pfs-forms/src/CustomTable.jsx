5c63a4546040d5c347b24f189ecd992564db509b
MasReed <bluekitdrummer@gmail.com>
2021-09-16 16:13:46 -0500

    create simple table layout; head/body/footer

--- pfs-forms/src/CustomTable.jsx
+++ pfs-forms/src/CustomTable.jsx
@@ -7,6 +7,9 @@ import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import TableFooter from '@material-ui/core/TableFooter'
+
+
 import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles((theme) => ({
@@ -14,6 +17,15 @@ const useStyles = makeStyles((theme) => ({
     margin: '3rem 0',
     padding: '1rem 0'
   },
+  head: {
+    backgroundColor: theme.palette.primary.main
+  },
+  body: {
+    backgroundColor: '#ccc'
+  },
+  footer: {
+    backgroundColor: theme.palette.secondary.main
+  }
 }))
 
 export default function CustomTable ({ rows, columnHeadings }) {
@@ -22,13 +34,32 @@ export default function CustomTable ({ rows, columnHeadings }) {
   return (
     <TableContainer className={classes.tableContainer}>
       <Table>
-        <TableHead>
+        {/* Table Header */}
+        <TableHead className={classes.head}>
           <TableRow>
             <TableCell>
-              ABCKD
+              HEAD
             </TableCell>
           </TableRow>
         </TableHead>
+
+        {/* Table body */}
+        <TableBody className={classes.body}>
+          <TableRow>
+            <TableCell>
+              BODY
+            </TableCell>
+          </TableRow>
+        </TableBody>
+
+        {/* Table Footer */}
+        <TableFooter className={classes.footer}>
+          <TableRow>
+            <TableCell>
+              FOOTER
+            </TableCell>
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   )
